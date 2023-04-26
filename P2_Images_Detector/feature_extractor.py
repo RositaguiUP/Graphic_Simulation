996c56520036e03138e1c2ac3c21f3c0330f3cf2
Rosita Aguirre Plascencia <0225352@up.edu.mx>
2023-04-25 22:28:49 -0600

    Updating features

--- P2_Images_Detector/feature_extractor.py
+++ P2_Images_Detector/feature_extractor.py
@@ -1,33 +1,46 @@
 # Load basic libraries.
 import cv2 
 
+def showImage(image) :  
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 # ****************** Features ******************
 
 def getContrast(img):
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     mean = cv2.mean(grayImg)[0]
     std_dev = cv2.meanStdDev(grayImg)[1][0][0]
-    contrast = std_dev / mean
+
+    contrast = 1
+    if std_dev == 0:
+        if mean == 255.0:
+            contrast = 0
+    else:
+        contrast = std_dev / mean
     return contrast
 
 def getBrightness(img):
     grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     mean = cv2.mean(grayImg)[0]
-    return mean
+    brightness = mean / 255.0
+    return brightness
 
 def getSaturation(img):
     hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
     h, s, v = cv2.split(hsvImg)
     mean = cv2.mean(s)[0]
-    return mean
+    saturation = mean / 255.0
+    return saturation
 
 # ****************** Main ******************
 
 def main():
-    image = cv2.imread('./Images/cats/cat1.jpg')
+    image = cv2.imread('./Images/tests/white.png')
     feat1 = getContrast(image)
     feat2 = getBrightness(image)
     feat3 = getSaturation(image)
